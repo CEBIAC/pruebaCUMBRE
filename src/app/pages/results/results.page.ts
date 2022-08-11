5c54bdf2b780d54e6fcdb9f7c57a9807b246ba37
LizethMartin <lizethalejandramartinezcamargo@gmail.com>
2022-08-11 17:00:36 -0500

    cambios resultados

--- src/app/pages/results/results.page.ts
+++ src/app/pages/results/results.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-results',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./results.page.scss'],
 })
 export class ResultsPage implements OnInit {
+  sections;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {
+    this.sections = 0;
   }
 
+  ngOnInit() {}
+
+  //Metodo para enviar a results
+  goNext() {
+    this.sections += 1;
+  }
 }
