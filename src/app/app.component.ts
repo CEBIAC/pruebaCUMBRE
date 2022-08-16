e522de81d9c7810717a36387792a6231009981e0
LizethMartin <lizethalejandramartinezcamargo@gmail.com>
2022-08-16 10:38:16 -0500

    resultados con promedios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,26 +33,33 @@ export class AppComponent {
     if (index == 14) {
       promedioDim = arr.reduce(reducer) / 6;
       this.promediosDim[index] = parseInt(promedioDim.toFixed(2));
+      this.arrPlaneación[index - 12] = arr.reduce(reducer) / 6;
+      console.log(this.arrPlaneación)
     } else {
       promedioDim = arr.reduce(reducer) / 4;
       this.promediosDim[index] = parseInt(promedioDim.toFixed(2));
       if (index == 11) {
         this.Capacidades = this.promediosDim.reduce(reducer) / 12;
         this.Capacidades = this.Capacidades.toFixed(2);
+        console.log(this.Capacidades)
       } else if (index > 11 && index <= 17) {
-        this.arrPlaneación[index - 11] = arr.reduce(reducer) / 4;
+        this.arrPlaneación[index - 12] = arr.reduce(reducer) / 4;
+        console.log(this.arrPlaneación)
         if (index == 17) {
-          this.Planeación = this.promediosDim.reduce(reducer) / 6;
+          this.Planeación = this.arrPlaneación.reduce(reducer) / 6;
           this.Planeación = this.Planeación.toFixed(2);
+          console.log(this.arrPlaneación,this.Planeación)
         }
       } else if (index > 17) {
-        this.arrSocial[index - 17] = arr.reduce(reducer) / 4;
+        this.arrSocial[index - 18] = arr.reduce(reducer) / 4;
+        console.log(this.arrSocial)
         if (index == 20) {
           //console.log(this.promediosDim)
           this.Social = this.arrSocial.reduce(reducer) / 3;
           this.Social = this.Social.toFixed(2);
           this.globalAverage = this.promediosDim.reduce(reducer) / 21;
           this.globalAverage = this.globalAverage.toFixed(2);
+          console.log(this.arrSocial,this.Social, this.globalAverage)
           this.saveResults(this.respuestasDim, this.promediosDim,this.Capacidades,this.Planeación,this.Social,this.globalAverage)
         }
       }
@@ -62,16 +69,16 @@ export class AppComponent {
 
 
   saveResults(answers, dimension,capacidades,planeacion,social, Average) {
-    this.results = dimension.push(capacidades,planeacion,social,Average);
-    console.log(this.results)
-    /*if (sessionStorage.getItem('user')) {
+    dimension.push(capacidades,planeacion,social, Average);
+    console.log('dimension:', dimension)
+    if (sessionStorage.getItem('user')) {
       this.user = JSON.parse(sessionStorage.getItem('user'));
       this.user.respuestas = JSON.stringify(answers);
-      this.user.resultados = JSON.stringify(this.results);
+      this.user.resultados = JSON.stringify(dimension);
       this.sapiolab.saveResults(this.user);
     } else {
       alert('Error almacenando datos');
-    }*/
+    }
 
 
   }
