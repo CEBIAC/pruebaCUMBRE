823aadb0548702a520363ca1be8a80995e871a5a
matbui <u6000223@unimilitar.edu.co>
2022-08-25 13:46:03 -0500

    boton descarga

--- src/app/pages/results/results.page.ts
+++ src/app/pages/results/results.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import jsPDF from "jspdf";
+import html2canvas from 'html2canvas';
+import * as jQuery from 'jquery';
+
+
 
 @Component({
   selector: 'app-results',
@@ -12,11 +17,89 @@ export class ResultsPage implements OnInit {
   constructor(private router: Router) {
     this.sections = 0;
   }
+  
 
+
+
+  public downloadPDF(): void {
+    const DATA1 = document.getElementById('parte1');
+    const DATA2 = document.getElementById('parte2');
+    const DATA3 = document.getElementById('parte3');
+    const DATA4 = document.getElementById('parte4');
+    const doc = new jsPDF('p','pt','a4');
+    doc.context2d.pageWrapYEnabled = true;
+    const options = {
+      background: "white",
+      scale:3,
+      pagesplit: true,
+    };
+    
+    html2canvas(DATA1, options).then((canvas) =>{
+      const img = canvas.toDataURL('./results.page.html');
+
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, 'resultados', 'FAST');
+      return doc;
+
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+    });
+
+
+    html2canvas(DATA2, options).then((canvas) =>{
+      const img = canvas.toDataURL('./results.page.html');
+
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, 'Capacidades para el emprendimiento', 'FAST');
+      return doc;
+
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+    });
+
+
+    html2canvas(DATA3, options).then((canvas) =>{
+      const img = canvas.toDataURL('./results.page.html');
+
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, 'Planeación', 'FAST');
+      return doc;
+
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+    });
+
+
+    html2canvas(DATA4, options).then((canvas) =>{
+      const img = canvas.toDataURL('./results.page.html');
+
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, 'Habilidades sociales ', 'FAST');
+      return doc;
+
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+    });
+  }
   ngOnInit() {}
 
+
   //Metodo para enviar a results
-  goNext() {
-    this.sections += 1;
-  }
+  
 }
